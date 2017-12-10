7ef2812807d921937826ef33631ddb02b0ab3527
Vitaliy <theyamshikov@yandex.ru>
2017-12-10 12:50:19 +0600

    make domain class

---
+++ src/domain/Domain.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace yii2module\vendor\domain;
+
+use yii2lab\domain\enums\Driver;
+
+class Domain extends \yii2lab\domain\Domain {
+	
+	public function config() {
+		return [
+			'repositories' => [
+				'generator' => Driver::FILE,
+			],
+			'services' => [
+				'generator' => [
+					'author' => 'Yamshikov Vitaliy',
+					'email' => 'theyamshikov@yandex.ru',
+					'ownerList' => [
+						'yii2lab',
+						'yii2module',
+						'yii2woop',
+					],
+				],
+			],
+		];
+	}
+	
+}
