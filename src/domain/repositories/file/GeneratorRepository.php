d6a1123992bdbb51e6bb956816456bd7e06f2f24
Vitaliy <theyamshikov@yandex.ru>
2017-12-11 19:49:43 +0600

    refacto

--- src/domain/repositories/file/GeneratorRepository.php
+++ src/domain/repositories/file/GeneratorRepository.php
@@ -1,29 +1,9 @@
 <?php
 
-namespace yii2module\vendor\domain\repositories\ar;
+namespace yii2module\vendor\domain\repositories\file;
 
-use yii2module\vendor\domain\models\Article;
-use yii2lab\domain\BaseEntity;
-use yii2lab\domain\repositories\ActiveArRepository;
+use yii2lab\domain\repositories\BaseRepository;
 
-class GeneratorRepository extends ActiveArRepository {
-
-	// todo:
-
-	public function uniqueFields() {
-		return [
-			['name'],
-			['title'],
-		];
-	}
-
-	protected function initQuery() {
-		$this->query = Article::find()->where(['is_deleted' => '0']);
-	}
-
-	public function delete(BaseEntity $entity) {
-		$entity->is_deleted = 1;
-		$this->update($entity);
-	}
+class GeneratorRepository extends BaseRepository {
 
 }
--- src/domain/services/GeneratorService.php
+++ src/domain/services/GeneratorService.php
@@ -8,36 +8,51 @@ use yii2lab\helpers\yii\FileHelper;
 
 class GeneratorService extends ActiveBaseService {
 
+    const DATA_PATH = 'vendor/yii2module/yii2-vendor/src/domain/data';
+
 	public $author;
 	public $email;
 	public $ownerList;
-	
+
 	public function generate($owner, $name) {
-		$config = [
-			'path' => 'vendor' . SL . $owner . SL . 'yii2-' . $name,
-			'full_name' => $owner . SL . $name,
-		];
-		$copy = new CopyFiles;
-		$copy->copyAllFiles('vendor/yii2module/yii2-vendor/src/domain/data', $config['path']);
-		$files = $copy->getFileList($config['path']);
+        $path = $this->getFullName($owner, $name);
+        $files = $this->copy($path);
 		foreach($files as $fileName1) {
-			$fileName = $config['path'] . DS . $fileName1;
-			$fileData = FileHelper::load($fileName);
-			$fileData = str_replace([
-				'{name}',
-				'{owner}',
-				'{author}',
-				'{email}',
-				'{year}',
-			], [
-				$name,
-				$owner,
-				$this->author,
-				$this->email,
-				date('Y'),
-			], $fileData);
-			FileHelper::save($fileName, $fileData);
+			$fileName = $path . DS . $fileName1;
+			$this->replace($name, $owner, $fileName);
 		}
 	}
+
+	private function getFullName($owner, $name) {
+        return 'vendor' . SL . $owner . SL . 'yii2-' . $name;
+    }
+
+    private function copy($to) {
+        $from = self::DATA_PATH;
+        $copy = new CopyFiles;
+        $copy->copyAllFiles($from, $to);
+        $files = $copy->getFileList($to);
+        return $files;
+    }
+
+    private function replace($name, $owner, $fileName) {
+        $fileData = FileHelper::load($fileName);
+        $fileData = $this->replaceData($name, $owner, $fileData);
+        FileHelper::save($fileName, $fileData);
+    }
+
+	private function replaceData($name, $owner, $data) {
+        $list = [
+            '{name}' => $name,
+            '{owner}' => $owner,
+            '{author}' => $this->author,
+            '{email}' => $this->email,
+            '{year}' => date('Y'),
+        ];
+        $search = array_keys($list);
+        $replace = array_values($list);
+        $data = str_replace($search, $replace, $data);
+        return $data;
+    }
 	
 }
