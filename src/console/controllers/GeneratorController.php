f6203453e0ca2f8fdd23ed581edf8622b2756d50
Vitaliy <theyamshikov@yandex.ru>
2017-12-18 20:59:58 +0600

    clean code

--- src/console/controllers/GeneratorController.php
+++ src/console/controllers/GeneratorController.php
@@ -21,43 +21,7 @@ class GeneratorController extends Controller
 		Output::block('Success generated');
 	}
 	
-	public function actionPackage()
-	{
-		list($owner, $name) = $this->inputPackage();
-		Yii::$app->vendor->generator->generatePackage($owner, $name);
-		Output::block('Package success generated');
-	}
-	
-	public function actionLicense()
-	{
-		list($owner, $name) = $this->inputPackage();
-		Yii::$app->vendor->generator->generateLicense($owner, $name);
-		Output::block('License success generated');
-	}
-	
-	public function actionGuide()
-	{
-		list($owner, $name) = $this->inputPackage();
-		Yii::$app->vendor->generator->generateGuide($owner, $name);
-		Output::block('Guide success generated');
-	}
-	
-	public function actionReadme()
-	{
-		list($owner, $name) = $this->inputPackage();
-		Yii::$app->vendor->generator->generateReadme($owner, $name);
-		Output::block('Readme success generated');
-	}
-	
-	public function actionTest()
-	{
-		list($owner, $name) = $this->inputPackage();
-		Yii::$app->vendor->generator->generateTest($owner, $name);
-		Output::block('Test success generated');
-	}
-	
 	private function inputPackage() {
-		return ['yii2woop', 'qwerty12345'];
 		$ownerSelect = Select::display('Select owner', Yii::$app->vendor->generator->ownerList);
 		$owner = Select::getFirstValue($ownerSelect);
 		$name = Enter::display('Enter vendor name');
