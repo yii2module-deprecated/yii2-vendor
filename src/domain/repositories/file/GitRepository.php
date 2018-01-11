ff85aa8fa9b4847e59673280b3f83ac295e95dc8
Vitaliy <theyamshikov@yandex.ru>
2018-01-11 11:20:30 +0600

    fix

--- src/domain/repositories/file/GitRepository.php
+++ src/domain/repositories/file/GitRepository.php
@@ -2,14 +2,7 @@
 
 namespace yii2module\vendor\domain\repositories\file;
 
-use yii\web\NotFoundHttpException;
-use yii2lab\domain\data\ArrayIterator;
-use yii2lab\domain\data\Query;
-use yii2lab\domain\interfaces\repositories\ReadInterface;
 use yii2lab\domain\repositories\BaseRepository;
-use yii2lab\misc\helpers\FilterHelper;
-use yii2module\rest_client\helpers\ArrayHelper;
-use yii2module\vendor\domain\entities\RepoEntity;
 use yii2module\vendor\domain\helpers\RepositoryHelper;
 
 class GitRepository extends BaseRepository {
@@ -17,7 +10,7 @@ class GitRepository extends BaseRepository {
 	public function pull($entity) {
 		$repo = RepositoryHelper::gitInstance($entity->package);
 		$result = $repo->pullWithInfo();
-		if($result = 'Already up-to-date.') {
+		if($result == 'Already up-to-date.') {
 			return false;
 		} else {
 			return $result;
