b7a97e54f2b0087ceb75da4ed844296667f1d8f0
Vitaliy <theyamshikov@yandex.ru>
2018-02-17 19:31:20 +0600

    update domain config

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -8,29 +8,40 @@ class Domain extends \yii2lab\domain\Domain {
 	
 	public function config() {
 		return [
-			'repositories' => [
-				'info' => Driver::FILE,
-				'package' => Driver::FILE,
-				'generator' => Driver::FILE,
-				'git' => Driver::FILE,
-				'test' => Driver::FILE,
-			],
-			'services' => [
-				'info',
-				'package',
-				'git',
-				'test',
-				'generator' => [
-					'author' => 'Yamshikov Vitaliy, WOOPPAY LLC',
-					'email' => 'theyamshikov@yandex.ru',
-					'owners' => [
-						'yii2lab',
-						'yii2module',
-						'yii2woop',
-						'yii2guide',
-					],
-				],
-			],
+            'repositories' => [
+                'info' => Driver::FILE,
+                'package' => Driver::FILE,
+                'generator' => Driver::FILE,
+                'git' => Driver::FILE,
+                'test' => Driver::FILE,
+            ],
+            'services' => [
+                'info' => [
+                    'ignore' => [
+                        'yii2module/yii2-test',
+                    ],
+                ],
+                'package' => [
+                    'aliases' => [
+                        '@root',
+                        '@vendor/yii2lab/yii2-application-template',
+                    ]
+                ],
+                'git',
+                'test' => [
+                    'aliases' => [],
+                ],
+                'generator' => [
+                    'author' => 'Yamshikov Vitaliy, WOOPPAY LLC',
+                    'email' => 'theyamshikov@yandex.ru',
+                    'owners' => [
+                        'yii2lab',
+                        'yii2module',
+                        'yii2woop',
+                        'yii2guide',
+                    ],
+                ],
+            ],
 		];
 	}
 	
