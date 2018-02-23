b15452a98d5e9d899eb6cdb121c2125e09638140
Vitaliy <theyamshikov@yandex.ru>
2018-02-23 11:23:06 +0600

    implements menu interface

--- src/admin/helpers/Menu.php
+++ src/admin/helpers/Menu.php
@@ -3,9 +3,10 @@
 namespace yii2module\vendor\admin\helpers;
 
 use common\enums\rbac\PermissionEnum;
+use yii2lab\helpers\interfaces\MenuInterface;
 use yii2lab\helpers\ModuleHelper;
 
-class Menu {
+class Menu implements MenuInterface {
 	
 	public function toArray() {
 		return [
