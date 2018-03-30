6eed3ea2b1e5d039feeaadebc4532189a0187952
Vitaliy <theyamshikov@yandex.ru>
2018-03-30 15:34:16 +0600

    move config to class

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -8,40 +8,42 @@ class Domain extends \yii2lab\domain\Domain {
 	
 	public function config() {
 		return [
-            'repositories' => [
-                'info' => Driver::FILE,
-                'package' => Driver::FILE,
-                'generator' => Driver::FILE,
-                'git' => Driver::FILE,
-                'test' => Driver::FILE,
-            ],
-            'services' => [
-                'info' => [
-                    'ignore' => [
-                        'yii2module/yii2-test',
-                    ],
-                ],
-                'package' => [
-                    'aliases' => [
-                        '@root',
-                        '@vendor/yii2lab/yii2-application-template',
-                    ]
-                ],
-                'git',
-                'test' => [
-                    'aliases' => [],
-                ],
-                'generator' => [
-                    'author' => 'Yamshikov Vitaliy, WOOPPAY LLC',
-                    'email' => 'theyamshikov@yandex.ru',
-                    'owners' => [
-                        'yii2lab',
-                        'yii2module',
-                        'yii2woop',
-                        'yii2guide',
-                    ],
-                ],
-            ],
+			'repositories' => [
+				'info' => Driver::FILE,
+				'package' => Driver::FILE,
+				'generator' => Driver::FILE,
+				'git' => Driver::FILE,
+				'test' => Driver::FILE,
+			],
+			'services' => [
+				'info' => [
+					'ignore' => [
+						'yii2module/yii2-test',
+					],
+				],
+				'package' => [
+					'aliases' => [
+						'@root',
+						'@vendor/yii2lab/yii2-application-template',
+					],
+				],
+				'git',
+				'test' => [
+					'aliases' => [
+						//'domain/v1/account',
+					],
+				],
+				'generator' => [
+					'author' => 'Yamshikov Vitaliy, WOOPPAY LLC',
+					'email' => 'theyamshikov@yandex.ru',
+					'owners' => [
+						'yii2lab',
+						'yii2module',
+						'yii2woop',
+						'yii2guide',
+					],
+				],
+			],
 		];
 	}
 	
