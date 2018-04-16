01a1478637de8ba17d944a88da494a2dcc321cae
Vitaliy <theyamshikov@yandex.ru>
2018-04-16 10:49:02 +0600

    use scenario

--- src/domain/repositories/file/GeneratorRepository.php
+++ src/domain/repositories/file/GeneratorRepository.php
@@ -2,22 +2,38 @@
 
 namespace yii2module\vendor\domain\repositories\file;
 
+use yii2lab\designPattern\scenario\helpers\ScenarioHelper;
 use yii2lab\domain\repositories\BaseRepository;
-use yii2lab\designPattern\command\helpers\CommandHelper;
 
 class GeneratorRepository extends BaseRepository {
 	
 	const GENERATOR_DIR = 'yii2module\vendor\domain\commands\generators\\';
 	const INSTALL_DIR = 'yii2module\vendor\domain\commands\install\\';
 	
+	/**
+	 * @param $config
+	 * @param $name
+	 *
+	 * @return mixed
+	 * @throws \yii\base\InvalidConfigException
+	 * @throws \yii\web\ServerErrorHttpException
+	 */
 	public function generate($config, $name) {
 		$config['class'] = self:: GENERATOR_DIR. $name;
-		return CommandHelper::run($config);
+		return ScenarioHelper::run($config);
 	}
 	
+	/**
+	 * @param $config
+	 * @param $name
+	 *
+	 * @return mixed
+	 * @throws \yii\base\InvalidConfigException
+	 * @throws \yii\web\ServerErrorHttpException
+	 */
 	public function install($config, $name) {
 		$config['class'] = self::INSTALL_DIR . $name;
-		return CommandHelper::run($config);
+		return ScenarioHelper::run($config);
 	}
 	
 }
