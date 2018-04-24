eac30e15c162de2104834d09e5d1145878d6371e
Vitaliy <theyamshikov@yandex.ru>
2018-04-24 19:31:58 +0600

    make sample api module

---
+++ src/domain/data/src/api/controllers/{Entity}Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace {owner}\{name}\api\controllers;
+
+use yii2lab\domain\rest\ActiveControllerWithQuery as Controller;
+
+class TransactionController extends Controller
+{
+	
+	public $serviceName = '{name}.{entity}';
+	
+}
