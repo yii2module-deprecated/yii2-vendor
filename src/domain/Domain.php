629aeb2bc4c26de44836b43401f7752d1f96284c
Vitaliy <theyamshikov@yandex.ru>
2018-07-25 23:58:51 +0600

    pretty

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -6,15 +6,15 @@ use yii2lab\domain\enums\Driver;
 
 /**
  * Class Domain
- *
+ * 
  * @package yii2module\vendor\domain
- *
  * @property \yii2module\vendor\domain\services\InfoService $info
  * @property \yii2module\vendor\domain\services\PackageService $package
  * @property \yii2module\vendor\domain\services\GitService $git
  * @property \yii2module\vendor\domain\services\TestService $test
  * @property \yii2module\vendor\domain\services\GeneratorService $generator
  * @property \yii2module\vendor\domain\services\PrettyService $pretty
+ * @property-read \yii2module\vendor\domain\interfaces\repositories\RepositoriesInterface $repositories
  */
 class Domain extends \yii2lab\domain\Domain {
 	
