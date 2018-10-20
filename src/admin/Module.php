55f50985a8b101f3ae0f4a68105e27c452ebd0b1
Vitaliy <theyamshikov@yandex.ru>
2018-10-20 23:52:04 +0600

    make autoload domain

--- src/admin/Module.php
+++ src/admin/Module.php
@@ -3,6 +3,7 @@
 namespace yii2module\vendor\admin;
 
 use yii\base\Module as YiiModule;
+use yii2lab\domain\helpers\DomainHelper;
 
 /**
  * welcome module definition class
@@ -11,5 +12,12 @@ class Module extends YiiModule
 {
 
 	public static $langDir = '@yii2module/vendor/domain/messages';
-
+	
+	public function init() {
+		DomainHelper::forgeDomains([
+			'vendor' => 'yii2module\vendor\domain\Domain',
+		]);
+		parent::init();
+	}
+	
 }
--- src/console/Module.php
+++ src/console/Module.php
@@ -3,11 +3,19 @@
 namespace yii2module\vendor\console;
 
 use yii\base\Module as YiiModule;
+use yii2lab\domain\helpers\DomainHelper;
 
 /**
  * offline module definition class
  */
 class Module extends YiiModule
 {
+	
+	public function init() {
+		DomainHelper::forgeDomains([
+			'vendor' => 'yii2module\vendor\domain\Domain',
+		]);
+		parent::init();
+	}
 	
 }
