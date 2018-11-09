15e92c56bee2a9a7ec9cce46a0f7b7b1e32792be
Vitaliy <theyamshikov@yandex.ru>
2018-11-09 15:09:47 +0600

    make action

--- src/console/controllers/PackageController.php
+++ src/console/controllers/PackageController.php
@@ -2,27 +2,32 @@
 
 namespace yii2module\vendor\console\controllers;
 
-use Yii;
 use yii2lab\extension\console\helpers\Alert;
 use yii2lab\extension\console\helpers\input\Enter;
 use yii2lab\extension\console\helpers\Output;
 use yii2lab\extension\console\base\Controller;
+use yii2lab\extension\package\helpers\ConfigHelper;
 use yii2lab\extension\package\helpers\PackageHelper;
 
-class PackageController extends Controller
-{
+class PackageController extends Controller {
 	
 	public function init() {
 		parent::init();
 		Output::line();
 	}
-
-	public function actionDownload()
-	{
-		$group = Enter::display('Enter vendor name');
-        $package = Enter::display('Enter package name');
-        PackageHelper::forge($group, $package);
-        Alert::success('Package downloaded and installed!');
+	
+	public function actionDownload() {
+		$group = Enter::display('Vendor name');
+		$package = Enter::display('Package name');
+		PackageHelper::forge($group, $package);
+		Alert::success('Package downloaded and installed!');
 	}
-
+	
+	public function actionInstall() {
+		$group = Enter::display('Vendor name');
+		$package = Enter::display('Package name');
+		ConfigHelper::addPackage($group, $package);
+		Alert::success('Package installed!');
+	}
+	
 }
