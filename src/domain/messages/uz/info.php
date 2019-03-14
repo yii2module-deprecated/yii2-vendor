fc0d071c4eb2a853516d67d3be795a1a55c76769
amubarak <amubarak@wooppay.com>
2019-03-14 16:58:49 +0600

    d

---
+++ src/domain/messages/uz/info.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+return [
+	'title' => 'Маҳаллий',
+	'list' => 'Барчаси',
+	'need_of_commit' => 'Белгиланди',
+	'has_changes' => 'Белгиланмаган ўзгаришлар мавжуд',
+	'no_found_changes' => 'Барча ўзгаришлар белгиланган',
+	'bat_success_generated' => 'Команда файли генерация қилинди. Бу ердан қидиринг: cmd/git/vendor pull.bat',
+	'generate_bat' => 'Пакетларни янгилаш учун команда файлини генерация қилиш',
+	'packages_success_pulled' => 'Пакетлар янгиланди',
+	'pull_packages' => 'Пакетларни янгилаш',
+	'has_readme' => 'README',
+	'has_changelog' => 'CHANGELOG',
+	'has_guide' => 'Қўлланма',
+	'has_license' => 'Лицензия',
+	'has_test' => 'Тестлар',
+	'list_changed' => 'Ўзгартирилганлар',
+	'list_for_release' => 'Релиз',
+	'last_commit' => 'HEAD коммит',
+	'new_commits' => 'Версияланмаган коммитлар',
+	'package_need_of_release' => 'Версияланмаган ўзгаришлар мавжуд. Навбатдаги релизни муомалага чиқариш мумкин.',
+	'draft_new_release' => 'Релизни муомалага чиқариш',
+	'required_packages' => 'Боғликликлар',
+	'package_has_changes' => 'Пакетда GIT ичига ёзилмаган ўзгаришлар мавжуд',
+];
---
+++ src/domain/messages/uz/main.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+return [
+	'title' => 'Composer',
+	'branch' => 'Шохобча',
+	'commit' => 'Комит',
+	'commits' => 'Комитлар',
+	'release' => 'Релиз',
+	'tag' => 'Тэг',
+	'tags' => 'Тэги',
+	'sha' => 'Хэш',
+];
